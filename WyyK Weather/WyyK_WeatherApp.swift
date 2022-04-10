b92135c50a87a8efde865c3d28ee15b86a453541
wykee2 <46722362+wykeenjenga@users.noreply.github.com>
2022-04-10 14:10:06 +0300

    Initial Commit

---
+++ WyyK Weather/WyyK_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WyyK_WeatherApp.swift
+//  WyyK Weather
+//
+//  Created by Wykee Njenga on 4/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WyyK_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
